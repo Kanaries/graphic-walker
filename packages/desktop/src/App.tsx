c64268c595c2328a8ca3b21775673364081864a9
observedobserver <270001151@qq.com>
2022-09-30 10:39:30 +0800

    feat: desktop with tauri

---
+++ packages/desktop/src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { GraphicWalker } from '@kanaries/graphic-walker'
+import "./App.css";
+
+function App() {
+
+  return <GraphicWalker />
+}
+
+export default App;
