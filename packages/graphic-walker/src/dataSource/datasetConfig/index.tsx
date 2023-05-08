149117959d9b30359d95a7ff3c56261be680ab27
k6sdevbob <dev.bob@kanaries.net>
2023-05-08 20:48:27 +0800

    feat(data table): data loader support
    Co-Authored-By: kyusho <antoineyang99@gmail.com>

--- packages/graphic-walker/src/dataSource/datasetConfig/index.tsx
+++ packages/graphic-walker/src/dataSource/datasetConfig/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import DatasetTable from "../../components/dataTable";
 import { observer } from "mobx-react-lite";
 import { useGlobalStore } from "../../store";
@@ -6,13 +6,31 @@ import { useGlobalStore } from "../../store";
 const DatasetConfig: React.FC = (props) => {
     const { commonStore, vizStore } = useGlobalStore();
     const { currentDataset } = commonStore;
-    const { dataSource, rawFields } = currentDataset;
+    const { dataLoader } = vizStore;
+    const [count, setCount] = useState(0);
+    useEffect(() => {
+        let isCurrent = true;
+        const task = dataLoader.stat(currentDataset);
+        task.then(d => {
+            if (isCurrent) {
+                setCount(d.count);
+            }
+        }).finally(() => {
+            isCurrent = false;
+        });
+        return () => {
+            setCount(0);
+            isCurrent = false;
+        };
+    }, [currentDataset]);
     return (
         <div>
-            <DatasetTable size={100} data={dataSource} metas={rawFields}
+            <DatasetTable size={100}
+                total={count}
                 onMetaChange={(fid, fIndex, diffMeta) => {
                     commonStore.updateCurrentDatasetMetas(fid, diffMeta)
                 }}
+                dataset={currentDataset}
             />
         </div>
     );
