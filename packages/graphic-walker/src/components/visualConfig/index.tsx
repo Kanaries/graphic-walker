968b64565388c834340cc5da8871a4ee9f098f73
fengyxz <763159242@qq.com>
2023-07-06 18:56:10 +0800

    fix: adjust dark mode

--- packages/graphic-walker/src/components/visualConfig/index.tsx
+++ packages/graphic-walker/src/components/visualConfig/index.tsx
@@ -75,7 +75,7 @@ const VisualConfigPanel: React.FC = (props) => {
                         <div className="mt-1">
                             <input
                                 type="text"
-                                className="block w-full rounded-md border-0 py-1 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                className="block w-full rounded-md border-0 py-1 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 dark:bg-zinc-900 dark:text-gray-500"
                                 value={format[fc] ?? ''}
                                 onChange={(e) => {
                                     setFormat((f) => ({
@@ -93,7 +93,7 @@ const VisualConfigPanel: React.FC = (props) => {
                     <div className="mt-1">
                         <input
                             type="text"
-                            className="block w-full text-gray-700 rounded-md border-0 py-1 px-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            className="block w-full text-gray-700 rounded-md border-0 py-1 px-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 dark:bg-zinc-900 dark:text-gray-500"
                             value={background ?? ''}
                             onChange={(e) => {
                                 setBackground(e.target.value);
