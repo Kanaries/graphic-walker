c2c1355a8e4eebc46ec0990fc5a3766f39460fad
Hao Chen <270001151@qq.com>
2023-07-27 15:42:38 +0800

    feat: support same field aggregate

--- packages/graphic-walker/src/vis/spec/aggregate.ts
+++ packages/graphic-walker/src/vis/spec/aggregate.ts
@@ -4,11 +4,12 @@ import { getMeaAggKey } from '../../utils';
 
 export function channelAggregate(encoding: { [key: string]: any }, fields: IViewField[]) {
     Object.values(encoding).forEach((c) => {
-        const targetField = fields.find((f) => f.fid === c.field && !('aggregate' in c));
+        if (c.aggregate === null) return;
+        const targetField = fields.find((f) => f.fid === c.field && (f.analyticType === 'measure' || f.fid === COUNT_FIELD_ID));
         if (targetField && targetField.fid === COUNT_FIELD_ID) {
             c.title = 'Count';
             c.field = getMeaAggKey(targetField.fid, targetField.aggName)
-        } else if (targetField && targetField.analyticType === 'measure') {
+        } else if (targetField) {
             c.title = `${targetField.aggName}(${targetField.name})`;
             c.field = getMeaAggKey(targetField.fid, targetField.aggName)
         }
