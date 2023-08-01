c0e04ab60525ba0dface60ef10e91a0aaf596410
islxyqwe <islxyqwe@kanaries.net>
2023-08-01 13:43:42 +0800

    fix: multi nest table

--- packages/graphic-walker/src/components/dataTable/index.tsx
+++ packages/graphic-walker/src/components/dataTable/index.tsx
@@ -1,10 +1,10 @@
-import React, { useMemo, useState } from "react";
-import styled from "styled-components";
-import { IMutField, IRow } from "../../interfaces";
-import { useTranslation } from "react-i18next";
-import Pagination from "./pagination";
-import { ChevronUpDownIcon } from "@heroicons/react/24/outline";
-import DropdownContext from "../dropdownContext";
+import React, { useMemo, useState } from 'react';
+import styled from 'styled-components';
+import { IMutField, IRow } from '../../interfaces';
+import { useTranslation } from 'react-i18next';
+import Pagination from './pagination';
+import { ChevronUpDownIcon } from '@heroicons/react/24/outline';
+import DropdownContext from '../dropdownContext';
 
 interface DataTableProps {
     size?: number;
@@ -32,31 +32,31 @@ const Container = styled.div`
         }
     }
 `;
-const ANALYTIC_TYPE_LIST = ["dimension", "measure"];
-const SEMANTIC_TYPE_LIST = ["nominal", "ordinal", "quantitative", "temporal"];
+const ANALYTIC_TYPE_LIST = ['dimension', 'measure'];
+const SEMANTIC_TYPE_LIST = ['nominal', 'ordinal', 'quantitative', 'temporal'];
 // function getCellType(field: IMutField): 'number' | 'text' {
 //     return field.dataType === 'number' || field.dataType === 'integer' ? 'number' : 'text';
 // }
-function getHeaderType(field: IMutField): "number" | "text" {
-    return field.analyticType === "dimension" ? "text" : "number";
+function getHeaderType(field: IMutField): 'number' | 'text' {
+    return field.analyticType === 'dimension' ? 'text' : 'number';
 }
 
 function getHeaderClassNames(field: IMutField) {
-    return field.analyticType === "dimension" ? "border-t-4 border-blue-400" : "border-t-4 border-purple-400";
+    return field.analyticType === 'dimension' ? 'border-t-4 border-blue-400' : 'border-t-4 border-purple-400';
 }
 
 function getSemanticColors(field: IMutField): string {
     switch (field.semanticType) {
-        case "nominal":
-            return "border border-transparent bg-sky-100 text-sky-800 dark:bg-sky-900 dark:text-sky-100 dark:border-sky-600";
-        case "ordinal":
-            return "border border-transparent bg-indigo-100 text-indigo-800 dark:bg-indigo-900 dark:text-indigo-100 dark:border-indigo-600";
-        case "quantitative":
-            return "border border-transparent bg-purple-100 text-purple-800 dark:bg-purple-900 dark:text-purple-100 dark:border-purple-600";
-        case "temporal":
-            return "border border-transparent bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-100 dark:border-yellow-600";
+        case 'nominal':
+            return 'border border-transparent bg-sky-100 text-sky-800 dark:bg-sky-900 dark:text-sky-100 dark:border-sky-600';
+        case 'ordinal':
+            return 'border border-transparent bg-indigo-100 text-indigo-800 dark:bg-indigo-900 dark:text-indigo-100 dark:border-indigo-600';
+        case 'quantitative':
+            return 'border border-transparent bg-purple-100 text-purple-800 dark:bg-purple-900 dark:text-purple-100 dark:border-purple-600';
+        case 'temporal':
+            return 'border border-transparent bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-100 dark:border-yellow-600';
         default:
-            return "border border-transparent bg-gray-400";
+            return 'border border-transparent bg-gray-400';
     }
 }
 
@@ -64,9 +64,9 @@ type wrapMutField = {
     colSpan: number;
     rowSpan: number;
 } & (
-    | { type: "field"; value: IMutField; fIndex: number }
+    | { type: 'field'; value: IMutField; fIndex: number }
     | {
-          type: "name";
+          type: 'name';
           value: string;
       }
 );
@@ -78,19 +78,21 @@ const getHeaders = (metas: IMutField[]): wrapMutField[][] => {
     metas.forEach((x, fIndex) => {
         const path = x.path ?? [x.name ?? x.fid];
         if (path.length > now) {
-            result[now - 1].push({
-                colSpan: 0,
-                rowSpan: 1,
-                type: "name",
-                value: path[path.length - 2],
-            });
+            for (let i = now - 1; i < path.length - 1; i++) {
+                result[i].push({
+                    colSpan: 0,
+                    rowSpan: 1,
+                    type: 'name',
+                    value: path[i],
+                });
+            }
         }
         now = path.length;
         for (let i = 0; i < path.length - 1; i++) {
             result[i][result[i].length - 1].colSpan++;
         }
         result[path.length - 1].push({
-            type: "field",
+            type: 'field',
             value: x,
             colSpan: 1,
             rowSpan: height - path.length + 1,
@@ -101,7 +103,7 @@ const getHeaders = (metas: IMutField[]): wrapMutField[][] => {
 };
 
 const getHeaderKey = (f: wrapMutField) => {
-    if (f.type === "name") {
+    if (f.type === 'name') {
         return f.value;
     }
     return f.value.name ?? f.value.fid;
@@ -156,22 +158,22 @@ const DataTable: React.FC<DataTableProps> = (props) => {
                                     colSpan={f.colSpan}
                                     rowSpan={f.rowSpan}
                                     key={getHeaderKey(f)}
-                                    className={"align-top"}
+                                    className={'align-top'}
                                 >
-                                    {f.type === "name" && (
+                                    {f.type === 'name' && (
                                         <div
                                             className={
-                                                "border-t-4 border-yellow-400 whitespace-nowrap py-3.5 text-left text-xs font-semibold text-gray-900 dark:text-gray-50"
+                                                'border-t-4 border-yellow-400 whitespace-nowrap py-3.5 text-left text-xs font-semibold text-gray-900 dark:text-gray-50'
                                             }
                                         >
                                             <b className="sticky inset-x-0 w-fit px-6 sm:pl-6">{f.value}</b>
                                         </div>
                                     )}
-                                    {f.type === "field" && (
+                                    {f.type === 'field' && (
                                         <div
                                             className={
                                                 getHeaderClassNames(f.value) +
-                                                " whitespace-nowrap py-3.5 px-6 text-left text-xs font-semibold text-gray-900 dark:text-gray-50 sm:pl-6"
+                                                ' whitespace-nowrap py-3.5 px-6 text-left text-xs font-semibold text-gray-900 dark:text-gray-50 sm:pl-6'
                                             }
                                         >
                                             <b>{f.value.basename || f.value.name || f.value.fid}</b>
@@ -180,16 +182,16 @@ const DataTable: React.FC<DataTableProps> = (props) => {
                                                     options={analyticTypeList}
                                                     onSelect={(value) => {
                                                         onMetaChange(f.value.fid, f.fIndex, {
-                                                            analyticType: value as IMutField["analyticType"],
+                                                            analyticType: value as IMutField['analyticType'],
                                                         });
                                                     }}
                                                 >
                                                     <span
                                                         className={
-                                                            "cursor-pointer inline-flex px-2.5 py-0.5 text-xs font-medium mt-1 rounded-full text-xs text-white " +
-                                                            (f.value.analyticType === "dimension"
-                                                                ? "bg-blue-500"
-                                                                : "bg-purple-500")
+                                                            'cursor-pointer inline-flex px-2.5 py-0.5 text-xs font-medium mt-1 rounded-full text-xs text-white ' +
+                                                            (f.value.analyticType === 'dimension'
+                                                                ? 'bg-blue-500'
+                                                                : 'bg-purple-500')
                                                         }
                                                     >
                                                         {f.value.analyticType}
@@ -202,13 +204,13 @@ const DataTable: React.FC<DataTableProps> = (props) => {
                                                     options={semanticTypeList}
                                                     onSelect={(value) => {
                                                         onMetaChange(f.value.fid, f.fIndex, {
-                                                            semanticType: value as IMutField["semanticType"],
+                                                            semanticType: value as IMutField['semanticType'],
                                                         });
                                                     }}
                                                 >
                                                     <span
                                                         className={
-                                                            "cursor-pointer inline-flex px-2.5 py-0.5 text-xs font-medium mt-1 rounded-full text-xs " +
+                                                            'cursor-pointer inline-flex px-2.5 py-0.5 text-xs font-medium mt-1 rounded-full text-xs ' +
                                                             getSemanticColors(f.value)
                                                         }
                                                     >
@@ -228,8 +230,8 @@ const DataTable: React.FC<DataTableProps> = (props) => {
                     {data.slice(from, to + 1).map((row, index) => (
                         <tr
                             className={
-                                "divide-x divide-gray-200 dark:divide-gray-700 " +
-                                (index % 2 ? "bg-gray-50 dark:bg-gray-900" : "")
+                                'divide-x divide-gray-200 dark:divide-gray-700 ' +
+                                (index % 2 ? 'bg-gray-50 dark:bg-gray-900' : '')
                             }
                             key={index}
                         >
@@ -238,7 +240,7 @@ const DataTable: React.FC<DataTableProps> = (props) => {
                                     key={field.fid + index}
                                     className={
                                         getHeaderType(field) +
-                                        " whitespace-nowrap py-2 pl-4 pr-3 text-xs text-gray-500 dark:text-gray-300 sm:pl-6"
+                                        ' whitespace-nowrap py-2 pl-4 pr-3 text-xs text-gray-500 dark:text-gray-300 sm:pl-6'
                                     }
                                 >
                                     {`${row[field.fid]}`}
