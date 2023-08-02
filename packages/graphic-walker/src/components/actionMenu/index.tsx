6109fa07057be243f36cb0d43f3c6b9b30238d59
AntoineYANG <dev.bob@kanaries.net>
2023-08-02 18:47:37 +0800

    feat: context menu

---
+++ packages/graphic-walker/src/components/actionMenu/index.tsx
@@ -0,0 +1,167 @@
+import React, { Fragment, type HTMLAttributes, memo, type ReactElement, createContext, useState, useContext, useRef, useCallback, type ComponentPropsWithoutRef } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { type ReactTag, type ElementType, useMenuButton } from './a11y';
+import ActionMenuItemList, { type IActionMenuItem } from "./list";
+
+
+interface IActionMenuContext {
+    disabled: boolean;
+    expanded: boolean;
+    moveTo(x: number, y: number): void;
+    open(): void;
+    close(): void;
+    _items: readonly IActionMenuItem[];
+}
+
+const Context = createContext<IActionMenuContext | null>(null);
+
+interface IActionMenuProps {
+    menu?: IActionMenuItem[];
+    disabled?: boolean;
+    /** @default false */
+    enableContextMenu?: boolean;
+    title?: string;
+}
+
+const ActionMenu: React.FC<IActionMenuProps & Omit<HTMLAttributes<HTMLDivElement>, keyof IActionMenuProps>> = (props) => {
+    const { menu = [], disabled = false, enableContextMenu = false, title, ...attrs } = props;
+
+    const [coord, setCoord] = useState<[x: number, y: number]>([0, 0]);
+    const buttonRef = useRef<HTMLButtonElement>(null);
+
+    if (disabled || menu.length === 0) {
+        return (
+            <div {...attrs}>
+                {props.children}
+            </div>
+        );
+    }
+
+    return (
+        <Menu as={Fragment}>
+            {({ open, close }) => {
+                return (
+                    <Context.Provider
+                        value={{
+                            disabled,
+                            expanded: open,
+                            moveTo(x, y) {
+                                setCoord([x, y]);
+                            },
+                            open() {
+                                if (!open) {
+                                    buttonRef.current?.click();
+                                }
+                            },
+                            close,
+                            _items: menu,
+                        }}
+                    >
+                        <Menu.Button
+                            ref={buttonRef}
+                            className="sr-only"
+                            aria-hidden
+                        />
+                        <div
+                            onContextMenu={e => {
+                                e.preventDefault();
+                                e.stopPropagation();
+                                setCoord([e.clientX, e.clientY]);
+                                if (!open) {
+                                    buttonRef.current?.click();
+                                }
+                            }}
+                            {...attrs}
+                        >
+                            {props.children}
+                        </div>
+                        {open && <div className="fixed inset-0 z-50 bg-transparent" aria-hidden="true" />}
+                        <Transition
+                            as={Fragment}
+                            enter="transition ease-out duration-100"
+                            enterFrom="transform opacity-0 scale-95"
+                            enterTo="transform opacity-100 scale-100"
+                            leave="transition ease-in duration-75"
+                            leaveFrom="transform opacity-100 scale-100"
+                            leaveTo="transform opacity-0 scale-95"
+                        >
+                            <Menu.Items
+                                className="fixed z-50 mt-0.5 min-w-[8rem] max-w-[16rem] origin-top-left bg-white dark:bg-zinc-900 shadow-lg border border-gray-50 dark:border-gray-800 focus:outline-none"
+                                style={{
+                                    left: coord[0],
+                                    top: coord[1],
+                                }}
+                            >
+                                <ActionMenuItemList title={title} items={menu} onDismiss={close} />
+                            </Menu.Items>
+                        </Transition>
+                    </Context.Provider>
+                );
+            }}
+        </Menu>
+    );
+};
+
+type IActionMenuButtonProps<T extends ReactTag> = (
+    | {
+        /** @default "button" */
+        as: T;
+    }
+    | {
+        /** @default "button" */
+        as?: ReactTag;
+    }
+) & {
+    onPress?: (ctx: IActionMenuContext | undefined) => void;
+    /** @deprecated use `onPress()` instead */
+    onClick?: () => void;
+};
+
+const ActionMenuButton = function ActionMenuButton<T extends ReactTag>(props: IActionMenuButtonProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof IActionMenuProps>): ReactElement {
+    const { as: _as = 'button', onPress, children, ...attrs } = props;
+    const Component = _as as T;
+
+    const ctx = useContext(Context);
+    const buttonRef = useRef<ElementType<T>>(null);
+
+    const handlePress = useCallback(() => {
+        if (ctx?.disabled) {
+            return;
+        }
+        const btn = buttonRef.current;
+        if (btn) {
+            const rect = (btn as unknown as HTMLElement | SVGElement).getBoundingClientRect();
+            ctx?.moveTo(rect.x + rect.width, rect.y);
+        }
+        if (onPress) {
+            return onPress(ctx ?? undefined);
+        }
+        if (ctx?.expanded) {
+            ctx.close();
+        } else {
+            ctx?.open();
+        }
+    }, [ctx, onPress]);
+
+    const buttonProps = useMenuButton({
+        ...attrs,
+        "aria-expanded": ctx?.expanded ?? false,
+        onPress: handlePress,
+    });
+
+    if (ctx?.disabled || !ctx?._items.length) {
+        return <div aria-hidden />;
+    }
+
+    return (
+        // @ts-expect-error Expression produces a union type that is too complex to represent
+        <Component {...buttonProps} ref={buttonRef}>
+            {children}
+        </Component>
+    );
+};
+
+
+export default Object.assign(ActionMenu, {
+    Button: memo(ActionMenuButton),
+});
--- packages/graphic-walker/src/fields/datasetFields/meaFields.tsx
+++ packages/graphic-walker/src/fields/datasetFields/meaFields.tsx
@@ -5,6 +5,8 @@ import { useGlobalStore } from "../../store";
 import DataTypeIcon from "../../components/dataTypeIcon";
 import { FieldPill } from "./fieldPill";
 import DropdownContext, { IDropdownContextOption } from "../../components/dropdownContext";
+import ActionMenu from "../../components/actionMenu";
+import { EllipsisVerticalIcon } from "@heroicons/react/24/outline";
 
 interface Props {
     provided: DroppableProvided;
@@ -49,37 +51,48 @@ const MeaFields: React.FC<Props> = (props) => {
                     {(provided, snapshot) => {
                         return (
                             <div className="block">
-                                <DropdownContext
-                                    disable={snapshot.isDragging}
-                                    options={MEA_ACTION_OPTIONS}
-                                    onSelect={(v, opIndex) => {
-                                        fieldActionHandler(v, opIndex, index);
-                                    }}
+                                <ActionMenu
+                                    menu={[]}
+                                    enableContextMenu
+                                    disabled={snapshot.isDragging}
                                 >
-                                    <FieldPill
-                                        className={`dark:text-white pt-0.5 pb-0.5 pl-2 pr-2 mx-0 m-1 text-xs hover:bg-purple-100 dark:hover:bg-purple-800 rounded-full truncate border border-transparent ${
-                                            snapshot.isDragging ? "bg-purple-100 dark:bg-purple-800" : ""
-                                        }`}
-                                        isDragging={snapshot.isDragging}
-                                        ref={provided.innerRef}
-                                        {...provided.draggableProps}
-                                        {...provided.dragHandleProps}
+
+                                    <DropdownContext
+                                        disable={snapshot.isDragging}
+                                        options={MEA_ACTION_OPTIONS}
+                                        onSelect={(v, opIndex) => {
+                                            fieldActionHandler(v, opIndex, index);
+                                        }}
                                     >
-                                        <DataTypeIcon dataType={f.semanticType} analyticType={f.analyticType} />{" "}
-                                        {f.name}&nbsp;
-                                    </FieldPill>
-                                </DropdownContext>
-                                {
-                                    <FieldPill
-                                        className={`dark:text-white pt-0.5 pb-0.5 pl-2 pr-2 mx-0 m-1 text-xs hover:bg-purple-100 dark:hover:bg-purple-800 rounded-full border-purple-400 border truncate ${
-                                            snapshot.isDragging ? "bg-purple-100 dark:bg-purple-800" : "hidden"
-                                        }`}
-                                        isDragging={snapshot.isDragging}
-                                    >
-                                        <DataTypeIcon dataType={f.semanticType} analyticType={f.analyticType} />{" "}
-                                        {f.name}&nbsp;
-                                    </FieldPill>
-                                }
+                                        <FieldPill
+                                            className={`dark:text-white pt-0.5 pb-0.5 pl-2 pr-2 mx-0 m-1 text-xs hover:bg-purple-100 dark:hover:bg-purple-800 rounded-full truncate border border-transparent ${snapshot.isDragging ? "bg-purple-100 dark:bg-purple-800" : ""
+                                                }`}
+                                            isDragging={snapshot.isDragging}
+                                            ref={provided.innerRef}
+                                            {...provided.draggableProps}
+                                            {...provided.dragHandleProps}
+                                        >
+                                            <DataTypeIcon dataType={f.semanticType} analyticType={f.analyticType} />
+                                            <span>{f.name}</span>
+                                            <ActionMenu.Button as="div">
+                                                <EllipsisVerticalIcon className="w-4 h-4" />
+                                            </ActionMenu.Button>
+                                        </FieldPill>
+                                    </DropdownContext>
+                                    {
+                                        <FieldPill
+                                            className={`dark:text-white pt-0.5 pb-0.5 pl-2 pr-2 mx-0 m-1 text-xs hover:bg-purple-100 dark:hover:bg-purple-800 rounded-full border-purple-400 border truncate ${snapshot.isDragging ? "bg-purple-100 dark:bg-purple-800" : "hidden"
+                                                }`}
+                                            isDragging={snapshot.isDragging}
+                                        >
+                                            <DataTypeIcon dataType={f.semanticType} analyticType={f.analyticType} />
+                                            <span>{f.name}</span>
+                                            <ActionMenu.Button as="div">
+                                                <EllipsisVerticalIcon className="w-4 h-4" />
+                                            </ActionMenu.Button>
+                                        </FieldPill>
+                                    }
+                                </ActionMenu>
                             </div>
                         );
                     }}
