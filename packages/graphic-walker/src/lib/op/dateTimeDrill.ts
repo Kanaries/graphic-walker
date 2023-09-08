882e93bf32464b73fc24c9e54009c1c52d04fa10
islxyqwe <islxyqwe123@gmail.com>
2023-09-08 11:53:38 +0800

    feat: new date drill level

--- packages/graphic-walker/src/lib/op/dateTimeDrill.ts
+++ packages/graphic-walker/src/lib/op/dateTimeDrill.ts
@@ -1,21 +1,30 @@
-import { DATE_TIME_DRILL_LEVELS } from "../../constants";
-import type { IExpParamter } from "../../interfaces";
-import type { IDataFrame } from "../execExp";
+import { DATE_TIME_DRILL_LEVELS } from '../../constants';
+import type { IExpParamter } from '../../interfaces';
+import type { IDataFrame } from '../execExp';
 
+const formatDate = (date: Date) => {
+    const Y = date.getFullYear();
+    const M = date.getMonth() + 1;
+    const D = date.getDate();
+    const H = date.getHours();
+    const m = date.getMinutes();
+    const s = date.getSeconds();
+    return `${Y}-${M}-${D} ${`${H}`.padStart(2, ' ')}:${`${m}`.padStart(2, '0')}:${`${s}`.padStart(2, '0')}`;
+};
 
 function dateTimeDrill(resKey: string, params: IExpParamter[], data: IDataFrame): IDataFrame {
-    const fieldKey = params.find(p => p.type === 'field')?.value;
-    const drillLevel = params.find(p => p.type === 'value')?.value as typeof DATE_TIME_DRILL_LEVELS[number] | undefined;
+    const fieldKey = params.find((p) => p.type === 'field')?.value;
+    const drillLevel = params.find((p) => p.type === 'value')?.value as (typeof DATE_TIME_DRILL_LEVELS)[number] | undefined;
     if (!fieldKey || !drillLevel) {
         return data;
     }
     const fieldValues = data[fieldKey];
     switch (drillLevel) {
         case 'year': {
-            const newValues = fieldValues.map(v => {
+            const newValues = fieldValues.map((v) => {
                 const date = new Date(v);
                 const Y = date.getFullYear();
-                return Y;
+                return formatDate(new Date(Y, 0, 1));
             });
             return {
                 ...data,
@@ -23,11 +32,11 @@ function dateTimeDrill(resKey: string, params: IExpParamter[], data: IDataFrame)
             };
         }
         case 'quarter': {
-            const newValues = fieldValues.map(v => {
+            const newValues = fieldValues.map((v) => {
                 const date = new Date(v);
                 const Y = date.getFullYear();
-                const Q = Math.floor(date.getMonth() / 3) + 1;
-                return `${Y} Q${Q}`;
+                const Q = Math.floor(date.getMonth() / 3);
+                return formatDate(new Date(Y, Q * 3, 1));
             });
             return {
                 ...data,
@@ -35,11 +44,11 @@ function dateTimeDrill(resKey: string, params: IExpParamter[], data: IDataFrame)
             };
         }
         case 'month': {
-            const newValues = fieldValues.map(v => {
+            const newValues = fieldValues.map((v) => {
                 const date = new Date(v);
                 const Y = date.getFullYear();
-                const M = date.getMonth() + 1;
-                return `${Y}-${M}`;
+                const M = date.getMonth();
+                return formatDate(new Date(Y, M, 1));
             });
             return {
                 ...data,
@@ -47,28 +56,14 @@ function dateTimeDrill(resKey: string, params: IExpParamter[], data: IDataFrame)
             };
         }
         case 'week': {
-            const newValues = fieldValues.map(v => {
+            const newValues = fieldValues.map((v) => {
                 const date = new Date(v);
                 const _Y = date.getFullYear();
                 const _firstDayOfYear = new Date(_Y, 0, 1);
-                const _SundayOfFirstWeek = new Date(
-                    new Date(_firstDayOfYear).setDate(_firstDayOfYear.getDate() - _firstDayOfYear.getDay())
-                );
+                const _SundayOfFirstWeek = new Date(new Date(_firstDayOfYear).setDate(_firstDayOfYear.getDate() - _firstDayOfYear.getDay()));
                 const Y = date.getTime() - _SundayOfFirstWeek.getTime() > 1_000 * 60 * 60 * 24 * 7 ? _Y : _SundayOfFirstWeek.getFullYear();
-                const firstDayOfYear = new Date(Y, 0, 1);
-                const SundayOfFirstWeek = new Date(
-                    new Date(firstDayOfYear).setDate(firstDayOfYear.getDate() - firstDayOfYear.getDay())
-                );
-                const W = Math.floor((date.getTime() - SundayOfFirstWeek.getTime()) / (7 * 24 * 60 * 60 * 1_000)) + 1;
-                const SundayOfThisWeek = new Date(
-                    new Date(date).setDate(date.getDate() - date.getDay())
-                );
-                const SaturdayOfThisWeek = new Date(
-                    SundayOfThisWeek.getTime() + 6 * 24 * 60 * 60 * 1_000
-                );
-                const beginDay = SundayOfThisWeek.toLocaleDateString();
-                const endDay = SaturdayOfThisWeek.toLocaleDateString();
-                return `${Y} W${W < 10 ? ' ' : ''}${W} (${beginDay} ~ ${endDay})`;
+                const SundayOfThisWeek = new Date(new Date(date).setDate(date.getDate() - date.getDay()));
+                return formatDate(SundayOfThisWeek);
             });
             return {
                 ...data,
@@ -76,12 +71,12 @@ function dateTimeDrill(resKey: string, params: IExpParamter[], data: IDataFrame)
             };
         }
         case 'day': {
-            const newValues = fieldValues.map(v => {
+            const newValues = fieldValues.map((v) => {
                 const date = new Date(v);
                 const Y = date.getFullYear();
-                const M = date.getMonth() + 1;
+                const M = date.getMonth();
                 const D = date.getDate();
-                return `${Y}-${M}-${D}`;
+                return formatDate(new Date(Y, M, D));
             });
             return {
                 ...data,
@@ -89,13 +84,13 @@ function dateTimeDrill(resKey: string, params: IExpParamter[], data: IDataFrame)
             };
         }
         case 'hour': {
-            const newValues = fieldValues.map(v => {
+            const newValues = fieldValues.map((v) => {
                 const date = new Date(v);
                 const Y = date.getFullYear();
-                const M = date.getMonth() + 1;
+                const M = date.getMonth();
                 const D = date.getDate();
                 const H = date.getHours();
-                return `${Y}-${M}-${D} ${H < 10 ? ' ' : ''}${H} o'clock`;
+                return formatDate(new Date(Y, M, D, H));
             });
             return {
                 ...data,
@@ -103,14 +98,14 @@ function dateTimeDrill(resKey: string, params: IExpParamter[], data: IDataFrame)
             };
         }
         case 'minute': {
-            const newValues = fieldValues.map(v => {
+            const newValues = fieldValues.map((v) => {
                 const date = new Date(v);
                 const Y = date.getFullYear();
-                const M = date.getMonth() + 1;
+                const M = date.getMonth();
                 const D = date.getDate();
                 const H = date.getHours();
                 const m = date.getMinutes();
-                return `${Y}-${M}-${D} ${H < 10 ? ' ' : ''}${H}:${m < 10 ? '0' : ''}${m}`;
+                return formatDate(new Date(Y, M, D, H, m));
             });
             return {
                 ...data,
@@ -118,15 +113,15 @@ function dateTimeDrill(resKey: string, params: IExpParamter[], data: IDataFrame)
             };
         }
         case 'second': {
-            const newValues = fieldValues.map(v => {
+            const newValues = fieldValues.map((v) => {
                 const date = new Date(v);
                 const Y = date.getFullYear();
-                const M = date.getMonth() + 1;
+                const M = date.getMonth();
                 const D = date.getDate();
                 const H = date.getHours();
                 const m = date.getMinutes();
                 const s = date.getSeconds();
-                return `${Y}-${M}-${D} ${H < 10 ? ' ' : ''}${H}:${m < 10 ? '0' : ''}${m}:${s < 10 ? '0' : ''}${s}`;
+                return formatDate(new Date(Y, M, D, H, m, s));
             });
             return {
                 ...data,
@@ -138,6 +133,5 @@ function dateTimeDrill(resKey: string, params: IExpParamter[], data: IDataFrame)
         }
     }
 }
-
 
 export default dateTimeDrill;
--- packages/graphic-walker/src/vis/spec/tooltip.ts
+++ packages/graphic-walker/src/vis/spec/tooltip.ts
@@ -1,17 +1,27 @@
-import { IViewField } from "../../interfaces";
-import { getMeaAggKey } from "../../utils";
+import { IViewField } from '../../interfaces';
+import { getMeaAggKey } from '../../utils';
 
-export function addTooltipEncode (encoding: {[key: string]: any}, details: Readonly<IViewField[]> = [], defaultAggregated = false) {
-    const encs = Object.keys(encoding).filter(ck => ck !== 'tooltip').map(ck => {
-        return {
-            field: encoding[ck].field,
-            type: encoding[ck].type,
-            title: encoding[ck].title
-        }
-    }).concat(details.map(f => ({
-        field: defaultAggregated ? getMeaAggKey(f.fid, f.aggName) : f.fid,
-        title: defaultAggregated && f.aggName ? `${f.aggName}(${f.name})` : f.name,
-        type: f.semanticType
-    })))
-    encoding.tooltip = encs
+export function addTooltipEncode(encoding: { [key: string]: any }, details: Readonly<IViewField[]> = [], defaultAggregated = false) {
+    const encs = Object.keys(encoding)
+        .filter((ck) => ck !== 'tooltip')
+        .map((ck) => {
+            return {
+                field: encoding[ck].field,
+                type: encoding[ck].type,
+                title: encoding[ck].title,
+                ...(encoding[ck].timeUnit
+                    ? {
+                          timeUnit: encoding[ck].timeUnit,
+                      }
+                    : {}),
+            };
+        })
+        .concat(
+            details.map((f) => ({
+                field: defaultAggregated ? getMeaAggKey(f.fid, f.aggName) : f.fid,
+                title: defaultAggregated && f.aggName ? `${f.aggName}(${f.name})` : f.name,
+                type: f.semanticType,
+            }))
+        );
+    encoding.tooltip = encs;
 }
