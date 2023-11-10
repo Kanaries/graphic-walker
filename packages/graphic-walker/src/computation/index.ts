0b786a0f11b59540a1872d8c334d8b8eaa3fcbfb
islxyqwe <islxyqwe123@gmail.com>
2023-11-10 12:19:01 +0800

    feat: filter Edit dialog optimize (#224)

--- packages/graphic-walker/src/computation/index.ts
+++ packages/graphic-walker/src/computation/index.ts
@@ -100,9 +100,22 @@ export const dataQuery = async (service: IComputationFunction, workflow: IDataQu
     return res;
 };
 
-export const fieldStat = async (service: IComputationFunction, field: IField, options: { values?: boolean; range?: boolean }): Promise<IFieldStats> => {
-    const { values = true, range = true } = options;
+export const fieldStat = async (
+    service: IComputationFunction,
+    field: IField,
+    options: {
+        values?: boolean;
+        range?: boolean;
+        valuesMeta?: boolean;
+        selectedCount?: Set<string | number>;
+        valuesLimit?: number;
+        valuesOffset?: number;
+        sortBy?: 'value' | 'value_dsc' | 'count' | 'count_dsc' | 'none';
+    }
+): Promise<IFieldStats> => {
+    const { values = true, range = true, valuesMeta = true, sortBy = 'none' } = options;
     const COUNT_ID = `count_${field.fid}`;
+    const TOTAL_DISTINCT_ID = `total_distinct_${field.fid}`;
     const MIN_ID = `min_${field.fid}`;
     const MAX_ID = `max_${field.fid}`;
     const transformWork: ITransformWorkflowStep[] = field.computed
@@ -118,6 +131,32 @@ export const fieldStat = async (service: IComputationFunction, field: IField, op
               },
           ]
         : [];
+    const valuesMetaQueryPayload: IDataQueryPayload = {
+        workflow: [
+            ...transformWork,
+            {
+                type: 'view',
+                query: [
+                    {
+                        op: 'aggregate',
+                        groupBy: [],
+                        measures: [
+                            {
+                                field: field.fid,
+                                agg: 'distinctCount',
+                                asFieldKey: TOTAL_DISTINCT_ID,
+                            },
+                            {
+                                field: '*',
+                                agg: 'count',
+                                asFieldKey: 'count',
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    };
     const valuesQueryPayload: IDataQueryPayload = {
         workflow: [
             ...transformWork,
@@ -137,8 +176,20 @@ export const fieldStat = async (service: IComputationFunction, field: IField, op
                     },
                 ],
             },
+            ...(sortBy === 'none'
+                ? []
+                : ([
+                      {
+                          type: 'sort',
+                          by: [sortBy.startsWith('value') ? field.fid : COUNT_ID],
+                          sort: sortBy.endsWith('dsc') ? 'descending' : 'ascending',
+                      },
+                  ] as ISortWorkflowStep[])),
         ],
+        limit: options.valuesLimit,
+        offset: options.valuesOffset,
     };
+    const [valuesMetaRes = { [TOTAL_DISTINCT_ID]: 0, count: 0 }] = valuesMeta ? await service(valuesMetaQueryPayload) : [{ [TOTAL_DISTINCT_ID]: 0, count: 0 }];
     const valuesRes = values ? await service(valuesQueryPayload) : [];
     const rangeQueryPayload: IDataQueryPayload = {
         workflow: [
@@ -180,14 +231,54 @@ export const fieldStat = async (service: IComputationFunction, field: IField, op
               },
           ];
 
+    const selectedCountWork: IDataQueryPayload | null = options.selectedCount?.size
+        ? {
+              workflow: [
+                  ...transformWork,
+                  {
+                      type: 'filter',
+                      filters: [
+                          {
+                              fid: field.fid,
+                              rule: {
+                                  type: 'one of',
+                                  value: [...options.selectedCount],
+                              },
+                          },
+                      ],
+                  },
+                  {
+                      type: 'view',
+                      query: [
+                          {
+                              op: 'aggregate',
+                              groupBy: [],
+                              measures: [
+                                  {
+                                      field: '*',
+                                      agg: 'count',
+                                      asFieldKey: 'count',
+                                  },
+                              ],
+                          },
+                      ],
+                  },
+              ],
+          }
+        : null;
+    const [selectedCountRes = { count: 0 }] = selectedCountWork ? await service(selectedCountWork) : [];
+
     return {
-        values: valuesRes
-            .sort((a, b) => b[COUNT_ID] - a[COUNT_ID])
-            .map((row) => ({
-                value: row[field.fid],
-                count: row[COUNT_ID],
-            })),
+        values: valuesRes.map((row) => ({
+            value: row[field.fid],
+            count: row[COUNT_ID],
+        })),
+        valuesMeta: {
+            total: valuesMetaRes.count,
+            distinctTotal: valuesMetaRes[TOTAL_DISTINCT_ID],
+        },
         range: [rangeRes[MIN_ID], rangeRes[MAX_ID]],
+        selectedCount: selectedCountRes.count,
     };
 };
 
