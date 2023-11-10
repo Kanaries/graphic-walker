320e9a781d0bc998bece013f8ac37b5b41c2c3e6
islxyqwe <islxyqwe123@gmail.com>
2023-11-10 12:32:51 +0800

    feat: filter with agg (#219)
    Warning: This feature requires Computation support multi view.

--- packages/graphic-walker/src/utils/workflow.ts
+++ packages/graphic-walker/src/utils/workflow.ts
@@ -11,7 +11,7 @@ import type {
     IFilterField,
 } from '../interfaces';
 import type { VizSpecStore } from '../store/visualSpecStore';
-import { getMeaAggKey } from '.';
+import { getFilterMeaAggKey, getMeaAggKey } from '.';
 import { MEA_KEY_ID, MEA_VAL_ID } from '../constants';
 
 const walkExpression = (expression: IExpression, each: (field: string) => void): void => {
@@ -73,15 +73,17 @@ export const toWorkflow = (
     let computedWorkflow: IFilterWorkflowStep | null = null;
     let viewQueryWorkflow: IViewWorkflowStep | null = null;
     let sortWorkflow: ISortWorkflowStep | null = null;
+    let aggFilterWorkflow: IFilterWorkflowStep | null = null;
 
     // TODO: apply **fold** before filter
 
     const createFilter = (f: IFilterField): IVisFilter => {
-        viewKeys.add(f.fid);
+        const fid = getFilterMeaAggKey(f);
+        viewKeys.add(fid);
         const rule = f.rule!;
         if (rule.type === 'one of') {
             return {
-                fid: f.fid,
+                fid,
                 rule: {
                     type: 'one of',
                     value: [...rule.value],
@@ -90,7 +92,7 @@ export const toWorkflow = (
         } else if (rule.type === 'temporal range') {
             const range = [new Date(rule.value[0]).getTime(), new Date(rule.value[1]).getTime()] as const;
             return {
-                fid: f.fid,
+                fid,
                 rule: {
                     type: 'temporal range',
                     value: range,
@@ -107,7 +109,7 @@ export const toWorkflow = (
         } else if (rule.type === 'range') {
             const range = [Number(rule.value[0]), Number(rule.value[1])] as const;
             return {
-                fid: f.fid,
+                fid,
                 rule: {
                     type: 'range',
                     value: range,
@@ -124,7 +126,7 @@ export const toWorkflow = (
     };
 
     // First, to apply filters on the detailed data
-    const filters = viewFilters.filter((f) => !f.computed && f.rule).map<IVisFilter>(createFilter);
+    const filters = viewFilters.filter((f) => !f.computed && f.rule && !f.enableAgg).map<IVisFilter>(createFilter);
     if (filters.length) {
         filterWorkflow = {
             type: 'filter',
@@ -150,7 +152,7 @@ export const toWorkflow = (
     }
 
     // Third, apply filter on the transformed data
-    const computedFilters = viewFilters.filter((f) => f.computed && f.rule).map<IVisFilter>(createFilter);
+    const computedFilters = viewFilters.filter((f) => f.computed && f.rule && !f.enableAgg).map<IVisFilter>(createFilter);
     if (computedFilters.length) {
         computedWorkflow = {
             type: 'filter',
@@ -162,7 +164,8 @@ export const toWorkflow = (
     // When aggregation is enabled, there're 2 cases:
     // 1. If any of the measures is aggregated, then we apply the aggregation
     // 2. If there's no measure in the view, then we apply the aggregation
-    const aggregateOn = viewMeasures.filter((f) => f.aggName).map((f) => [f.fid, f.aggName as string]);
+    const aggergatedFilter = viewFilters.filter((f) => f.enableAgg && f.aggName && f.rule);
+    const aggregateOn = viewMeasures.filter((f) => f.aggName).map((f) => [f.fid, f.aggName as string]).concat(aggergatedFilter.map(f => [f.fid, f.aggName as string]));
     const aggergated = defaultAggregated && (aggregateOn.length || (viewMeasures.length === 0 && viewDimensions.length > 0));
 
     if (aggergated) {
@@ -172,7 +175,7 @@ export const toWorkflow = (
                 {
                     op: 'aggregate',
                     groupBy: viewDimensions.map((f) => f.fid),
-                    measures: viewMeasures.map((f) => ({
+                    measures: viewMeasures.concat(aggergatedFilter).map((f) => ({
                         field: f.fid,
                         agg: f.aggName as any,
                         asFieldKey: getMeaAggKey(f.fid, f.aggName!),
@@ -192,6 +195,14 @@ export const toWorkflow = (
         };
     }
 
+    // Apply aggregation Filter after aggregation.
+    if (aggergated && viewDimensions.length > 0 && aggergatedFilter.length > 0) {
+        aggFilterWorkflow = {
+            type: 'filter',
+            filters: aggergatedFilter.map(createFilter),
+        };
+    }
+
     if (sort !== 'none' && limit) {
         sortWorkflow = {
             type: 'sort',
@@ -200,7 +211,7 @@ export const toWorkflow = (
         };
     }
 
-    const steps: IDataQueryWorkflowStep[] = [filterWorkflow!, transformWorkflow!, computedWorkflow!, viewQueryWorkflow!, sortWorkflow!].filter(Boolean);
+    const steps: IDataQueryWorkflowStep[] = [filterWorkflow!, transformWorkflow!, computedWorkflow!, viewQueryWorkflow!, aggFilterWorkflow!, sortWorkflow!].filter(Boolean);
     return steps;
 };
 
