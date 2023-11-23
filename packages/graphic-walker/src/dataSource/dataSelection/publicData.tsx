850b3dac7026e99c8601cbfc2d6e855ca2763cc2
unimu-cic <kkcarson1874@gmail.com>
2023-11-23 17:09:09 +0800

    feat: improve interaction of Public Datasets selector
    - Users now receive immediate feedback and reduce confusion when selecting datasets.

--- packages/graphic-walker/src/dataSource/dataSelection/publicData.tsx
+++ packages/graphic-walker/src/dataSource/dataSelection/publicData.tsx
@@ -1,56 +1,83 @@
 import React from 'react';
 import Table from '../table';
-import { DemoDataAssets, PUBLIC_DATA_LIST } from '../config'
-import { useGlobalStore } from '../../store';
-import { observer } from 'mobx-react-lite';
-import { useTranslation } from 'react-i18next';
+import {DemoDataAssets, PUBLIC_DATA_LIST, IPublicData} from '../config';
+import {useGlobalStore} from '../../store';
+import {observer} from 'mobx-react-lite';
+import {useTranslation} from 'react-i18next';
+import {classNames} from '../../utils';
 import PrimaryButton from '../../components/button/primary';
-
+import {RadioGroup} from '@headlessui/react';
+import {CheckCircleIcon} from '@heroicons/react/24/solid';
 
 interface IPublicDataProps {
 
 }
 
 const PublicData: React.FC<IPublicDataProps> = props => {
-    const commonStore= useGlobalStore();
-    const { tmpDataSource } = commonStore;
-    const { t } = useTranslation('translation', { keyPrefix: 'DataSource.dialog.public' });
+	const commonStore = useGlobalStore();
+	const {tmpDataSource} = commonStore;
+	const {t} = useTranslation('translation', {keyPrefix: 'DataSource.dialog.public'});
 
-    return <div>
-        <div className="h-48 overflow-auto mb-1">
-            {
-                PUBLIC_DATA_LIST.map(data => <div key={data.key}
-                    onClick={() => {
-                        fetch(DemoDataAssets[data.key]).then(res => res.json())
-                        .then(res => {
-                            commonStore.updateTempSTDDS({
-                                dataSource: res.dataSource,
-                                rawFields: res.fields.map(f => ({
-                                    fid: f.fid,
-                                    name: f.name,
-                                    analyticType: f.analyticType,
-                                    semanticType: f.semanticType,
-                                    dataType: f.dataType || '?'
-                                })),
-                                name: data.title
-                            })
-                        })
-                    }}
-                    className="border rounded border-gray-300 dark:border-gray-600 p-2 m-2 cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-800 dark:text-gray-200"
-                    >
-                <div>{data.title}</div>
-                {/* <p>{data.title}</p> */}
-            </div>)
-            }
-        </div>
-        <PrimaryButton
-            className='my-1'
-            disabled={tmpDataSource.length === 0}
-            onClick={() => { commonStore.commitTempDS() }}
-            text={t('submit')}
-        />
-        <Table />
-    </div>
-}
+	const handleDataKeyChange = async (data: IPublicData) => {
+		try {
+			const response = await fetch(DemoDataAssets[data.key]);
+			const res = await response.json();
+			commonStore.updateTempSTDDS({
+				dataSource: res.dataSource,
+				rawFields: res.fields.map((f) => ({
+					fid: f.fid,
+					name: f.name,
+					analyticType: f.analyticType,
+					semanticType: f.semanticType,
+					dataType: f.dataType || '?',
+				})),
+				name: data.title,
+			});
+		} catch (error) {
+			// TODO: add error notification.
+			console.error('Error fetching public data:', error);
+		}
+	};
+
+	return (
+		<div>
+			<RadioGroup className="h-48 overflow-auto mb-1" by="key" onChange={handleDataKeyChange}>
+				{PUBLIC_DATA_LIST.map((data) => (
+					<RadioGroup.Option
+						key={data.key}
+						value={data}
+						className={({active, checked}) =>
+							classNames(
+								'flex focus:outline-none border rounded items-center justify-between border-gray-300 dark:border-gray-600 p-2 m-2 cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-800 dark:text-gray-200',
+								active ? 'ring-2 ring-offset-2 ring-indigo-500' : '',
+								checked ? 'bg-gray-50 dark:bg-gray-800 dark:text-gray-200' : '',
+							)
+						}
+					>
+						{({checked}) => (
+							<>
+								<RadioGroup.Label as="p">{data.title}</RadioGroup.Label>
+								{checked && (
+									<div className="shrink-0 text-indigo-600">
+										<CheckCircleIcon className="w-5 h-5"/>
+									</div>
+								)}
+							</>
+						)}
+					</RadioGroup.Option>
+				))}
+			</RadioGroup>
+			<PrimaryButton
+				className="my-1"
+				disabled={tmpDataSource.length === 0}
+				onClick={() => {
+					commonStore.commitTempDS();
+				}}
+				text={t('submit')}
+			/>
+			<Table/>
+		</div>
+	);
+};
 
 export default observer(PublicData);
