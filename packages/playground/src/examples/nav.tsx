60d36a533d6009ae285dc7787a112c6255626516
islxyqwe <islxyqwe123@gmail.com>
2023-12-04 03:40:29 +0800

    feat: add examples

---
+++ packages/playground/src/examples/nav.tsx
@@ -0,0 +1,19 @@
+export const pages = [
+  {
+    comp: () => import("./pages/gw"),
+    name: "GraphicWalker",
+  },
+  {
+    comp: () => import("./pages/pureRenderer"),
+    name: "PureRenderer",
+  },
+  {
+    comp: () => import("./pages/inModal"),
+    name: "Graphic Walker In a Modal",
+  },
+].map((Component) => ({
+  name: Component.name,
+  path: Component.name.replace(/\s/g, "_"),
+  lazy: async () =>
+    Component.comp().then((mod) => ({ Component: mod.default })),
+}));
---
+++ packages/playground/src/examples/util.ts
@@ -0,0 +1,43 @@
+import { IMutField, IRow } from "@kanaries/graphic-walker";
+export function promiseWrapper<T>(promise: Promise<T>): () => T {
+  let status = "pending";
+  let result: T;
+
+  const s = promise.then(
+    (value) => {
+      status = "success";
+      result = value;
+    },
+    (error) => {
+      status = "error";
+      result = error;
+    }
+  );
+
+  return () => {
+    switch (status) {
+      case "pending":
+        throw s;
+      case "success":
+        return result;
+      case "error":
+        throw result;
+      default:
+        throw new Error("Unknown status");
+    }
+  };
+}
+
+const cache: Map<string, () => unknown> = new Map();
+
+export function useFetch<T>(url: string): T {
+  if (!cache.has(url)) {
+    cache.set(url, promiseWrapper(fetch(url).then((resp) => resp.json() as T)));
+  }
+  return cache.get(url)!() as T;
+}
+
+export interface IDataSource {
+  dataSource: IRow[];
+  fields: IMutField[];
+}
