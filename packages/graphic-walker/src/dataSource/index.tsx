d5a52e8d2106d6293ede199b8df572bd4777c97a
islxyqwe <islxyqwe123@gmail.com>
2024-01-17 18:37:14 +0800

    fix: data source text

--- packages/graphic-walker/src/dataSource/index.tsx
+++ packages/graphic-walker/src/dataSource/index.tsx
@@ -29,7 +29,7 @@ const DataSourceSegment: React.FC<DSSegmentProps> = observer((props) => {
 
     const { showDSPanel } = commonStore;
     return (
-        <div className="flex items-center m-4 p-4 border border-gray-200 dark:border-gray-700">
+        <div className="font-sans flex items-center m-4 p-4 border border-gray-200 dark:border-gray-700">
             {props.onLoad && <GwFile onImport={props.onLoad} fileRef={gwFileRef} />}
             {/* <label className="text-xs mr-1 whitespace-nowrap self-center h-4">
                 {t("DataSource.labels.cur_dataset")}
