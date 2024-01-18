7d71de4120ccfaeedace7edbc3c7360fc71b6260
islxyqwe <islxyqwe123@gmail.com>
2024-01-18 15:43:15 +0800

    fix: timezone

--- packages/graphic-walker/src/components/visualConfig/timezone.ts
+++ packages/graphic-walker/src/components/visualConfig/timezone.ts
@@ -1,154 +1,80 @@
 export const timezones = [
+    { value: 720, name: '(UTC-12:00) Dateline Standard Time' },
+    { value: 660, name: '(UTC-11:00) Niue Time, Samoa Standard Time' },
+    { value: 600, name: '(UTC-10:00) Cook Is. Time, Hawaii Standard Time, Tahiti Time' },
+    { value: 570, name: '(UTC-09:30) Marquesas Time' },
+    { value: 540, name: '(UTC-09:00) Gambier Time, Hawaii Daylight Time' },
+    { value: 480, name: '(UTC-08:00) Alaska Daylight Time, Pacific Standard Time, Pitcairn Standard Time' },
+    { value: 420, name: '(UTC-07:00) Mountain Standard Time, Pacific Daylight Time' },
+    { value: 360, name: '(UTC-06:00) Central Standard Time, Galapagos Time, Mountain Daylight Time' },
+    { value: 300, name: '(UTC-05:00) Acre Time, Central Daylight Time, Colombia Time, Easter Is. Time, Eastern Standard Time, Ecuador Time, Peru Time' },
+    { value: 270, name: '(UTC-04:30) Venezuela Time' },
     {
-        "name": "(UTC-12:00) Etc/GMT+12",
-        "value": 720
+        value: 240,
+        name: '(UTC-04:00) Amazon Time, Atlantic Standard Time, Bolivia Time, Cuba Daylight Time, Eastern Daylight Time, Guyana Time, Paraguay Time',
     },
     {
-        "name": "(UTC-11:00) Etc/GMT+11, Pacific/Midway, Pacific/Niue, Pacific/Pago_Pago, Pacific/Samoa, US/Samoa",
-        "value": 660
+        value: 180,
+        name: '(UTC-03:00) Argentine Time, Atlantic Daylight Time, Brasilia Time, Chile Time, Falkland Is. Time, French Guiana Time, Rothera Time, Suriname Time, Uruguay Time',
+    },
+    { value: 150, name: '(UTC-02:30) Newfoundland Daylight Time' },
+    {
+        value: 120,
+        name: '(UTC-02:00) Fernando de Noronha Time, Pierre & Miquelon Daylight Time, South Georgia Standard Time, Western Greenland Summer Time',
+    },
+    { value: 60, name: '(UTC-01:00) Cape Verde Time' },
+    { value: 0, name: '(UTC) Azores Summer Time, Coordinated Universal Time, Eastern Greenland Summer Time, Ghana Mean Time, Greenwich Mean Time' },
+    { value: -60, name: '(UTC+01:00) British Summer Time, Central European Time, Irish Summer Time, Western African Time, Western European Summer Time' },
+    {
+        value: -120,
+        name: '(UTC+02:00) Central African Time, Central European Summer Time, Eastern European Time, Middle Europe Summer Time, South Africa Standard Time',
     },
     {
-        "name": "(UTC-10:00) America/Adak, America/Atka, Etc/GMT+10, HST, Pacific/Honolulu, Pacific/Johnston, Pacific/Rarotonga, Pacific/Tahiti, US/Aleutian, US/Hawaii",
-        "value": 600
+        value: -180,
+        name: '(UTC+03:00) Arabia Standard Time, Eastern African Time, Eastern European Summer Time, Israel Daylight Time, Moscow Standard Time, Syowa Time',
+    },
+    { value: -240, name: '(UTC+04:00) Armenia Time, Georgia Time, Gulf Standard Time, Mauritius Time, Reunion Time, Samara Time, Seychelles Time' },
+    { value: -270, name: '(UTC+04:30) Afghanistan Time, Iran Daylight Time' },
+    {
+        value: -300,
+        name: '(UTC+05:00) Aqtau Time, Aqtobe Time, Azerbaijan Summer Time, French Southern & Antarctic Lands Time, Maldives Time, Mawson Time, Oral Time, Pakistan Time, Tajikistan Time, Turkmenistan Time, Uzbekistan Time, Yekaterinburg Time',
+    },
+    { value: -330, name: '(UTC+05:30) India Standard Time' },
+    { value: -345, name: '(UTC+05:45) Nepal Time' },
+    {
+        value: -360,
+        name: '(UTC+06:00) Alma-Ata Time, Bangladesh Time, Bhutan Time, Indian Ocean Territory Time, Kirgizstan Time, Novosibirsk Time, Omsk Time, Qyzylorda Time, Vostok Time, Xinjiang Standard Time',
+    },
+    { value: -390, name: '(UTC+06:30) Cocos Islands Time, Myanmar Time' },
+    { value: -420, name: '(UTC+07:00) Christmas Island Time, Davis Time, Indochina Time, Krasnoyarsk Time, West Indonesia Time' },
+    {
+        value: -480,
+        name: '(UTC+08:00) Australian Western Standard Time, Brunei Time, Central Indonesia Time, China Standard Time, Hong Kong Time, Hovd Summer Time, Irkutsk Time, Malaysia Time, Philippines Time, Singapore Time',
+    },
+    { value: -525, name: '(UTC+08:45) Australian Central Western Standard Time' },
+    {
+        value: -540,
+        name: '(UTC+09:00) Choibalsan Summer Time, East Indonesia Time, Japan Standard Time, Khandyga Time, Korea Standard Time, Palau Time, Timor-Leste Time, Ulaanbaatar Summer Time, Yakutsk Time',
     },
     {
-        "name": "(UTC-09:30) Pacific/Marquesas",
-        "value": 570
+        value: -570,
+        name: '(UTC+09:30) Australian Central Standard Time (Northern Territory), Australian Central Standard Time (South Australia), Australian Central Standard Time (South Australia/New South Wales)',
     },
     {
-        "name": "(UTC-09:00) America/Anchorage, America/Juneau, America/Metlakatla, America/Nome, America/Sitka, America/Yakutat, Etc/GMT+9, Pacific/Gambier, US/Alaska",
-        "value": 540
+        value: -600,
+        name: "(UTC+10:00) Australian Eastern Standard Time (New South Wales), Australian Eastern Standard Time (Queensland), Australian Eastern Standard Time (Tasmania), Australian Eastern Standard Time (Victoria), Chamorro Standard Time, Chuuk Time, Dumont-d'Urville Time, Magadan Time, Papua New Guinea Time, Sakhalin Time, Ust-Nera Time, Vladivostok Time",
     },
+    { value: -630, name: '(UTC+10:30) Lord Howe Standard Time' },
     {
-        "name": "(UTC-08:00) America/Ensenada, America/Los_Angeles, America/Santa_Isabel, America/Tijuana, America/Vancouver, Canada/Pacific, Etc/GMT+8, Mexico/BajaNorte, PST8PDT, Pacific/Pitcairn, US/Pacific",
-        "value": 480
+        value: -660,
+        name: '(UTC+11:00) Bougainville Standard Time, Kosrae Time, Macquarie Island Standard Time, New Caledonia Time, Pohnpei Time, Solomon Is. Time, Srednekolymsk Time, Vanuatu Time',
     },
+    { value: -690, name: '(UTC+11:30) Norfolk Time' },
     {
-        "name": "(UTC-07:00) America/Boise, America/Cambridge_Bay, America/Ciudad_Juarez, America/Creston, America/Dawson, America/Dawson_Creek, America/Denver, America/Edmonton, America/Fort_Nelson, America/Hermosillo, America/Inuvik, America/Mazatlan, America/Phoenix, America/Shiprock, America/Whitehorse, America/Yellowknife, Canada/Mountain, Canada/Yukon, Etc/GMT+7, MST, MST7MDT, Mexico/BajaSur, Navajo, US/Arizona, US/Mountain",
-        "value": 420
+        value: -720,
+        name: '(UTC+12:00) Anadyr Time, Fiji Time, Gilbert Is. Time, Marshall Islands Time, Nauru Time, New Zealand Standard Time, Petropavlovsk-Kamchatski Time, Tuvalu Time, Wake Time, Wallis & Futuna Time',
     },
-    {
-        "name": "(UTC-06:00) America/Bahia_Banderas, America/Belize, America/Chicago, America/Chihuahua, America/Costa_Rica, America/El_Salvador, America/Guatemala, America/Indiana/Knox, America/Indiana/Tell_City, America/Knox_IN, America/Managua, America/Matamoros, America/Menominee, America/Merida, America/Mexico_City, America/Monterrey, America/North_Dakota/Beulah, America/North_Dakota/Center, America/North_Dakota/New_Salem, America/Ojinaga, America/Rainy_River, America/Rankin_Inlet, America/Regina, America/Resolute, America/Swift_Current, America/Tegucigalpa, America/Winnipeg, CST6CDT, Canada/Central, Canada/Saskatchewan, Etc/GMT+6, Mexico/General, Pacific/Galapagos, US/Central, US/Indiana-Starke",
-        "value": 360
-    },
-    {
-        "name": "(UTC-05:00) America/Atikokan, America/Bogota, America/Cancun, America/Cayman, America/Coral_Harbour, America/Detroit, America/Eirunepe, America/Fort_Wayne, America/Grand_Turk, America/Guayaquil, America/Havana, America/Indiana/Indianapolis, America/Indiana/Marengo, America/Indiana/Petersburg, America/Indiana/Vevay, America/Indiana/Vincennes, America/Indiana/Winamac, America/Indianapolis, America/Iqaluit, America/Jamaica, America/Kentucky/Louisville, America/Kentucky/Monticello, America/Lima, America/Louisville, America/Montreal, America/Nassau, America/New_York, America/Nipigon, America/Panama, America/Pangnirtung, America/Port-au-Prince, America/Porto_Acre, America/Rio_Branco, America/Thunder_Bay, America/Toronto, Brazil/Acre, Canada/Eastern, Chile/EasterIsland, Cuba, EST, EST5EDT, Etc/GMT+5, Jamaica, Pacific/Easter, US/East-Indiana, US/Eastern, US/Michigan",
-        "value": 300
-    },
-    {
-        "name": "(UTC-04:00) America/Anguilla, America/Antigua, America/Aruba, America/Barbados, America/Blanc-Sablon, America/Boa_Vista, America/Campo_Grande, America/Caracas, America/Cuiaba, America/Curacao, America/Dominica, America/Glace_Bay, America/Goose_Bay, America/Grenada, America/Guadeloupe, America/Guyana, America/Halifax, America/Kralendijk, America/La_Paz, America/Lower_Princes, America/Manaus, America/Marigot, America/Martinique, America/Moncton, America/Montserrat, America/Port_of_Spain, America/Porto_Velho, America/Puerto_Rico, America/Santo_Domingo, America/St_Barthelemy, America/St_Kitts, America/St_Lucia, America/St_Thomas, America/St_Vincent, America/Thule, America/Tortola, America/Virgin, Atlantic/Bermuda, Brazil/West, Canada/Atlantic, Etc/GMT+4",
-        "value": 240
-    },
-    {
-        "name": "(UTC-03:30) America/St_Johns, Canada/Newfoundland",
-        "value": 210
-    },
-    {
-        "name": "(UTC-03:00) America/Araguaina, America/Argentina/Buenos_Aires, America/Argentina/Catamarca, America/Argentina/ComodRivadavia, America/Argentina/Cordoba, America/Argentina/Jujuy, America/Argentina/La_Rioja, America/Argentina/Mendoza, America/Argentina/Rio_Gallegos, America/Argentina/Salta, America/Argentina/San_Juan, America/Argentina/San_Luis, America/Argentina/Tucuman, America/Argentina/Ushuaia, America/Asuncion, America/Bahia, America/Belem, America/Buenos_Aires, America/Catamarca, America/Cayenne, America/Cordoba, America/Fortaleza, America/Jujuy, America/Maceio, America/Mendoza, America/Miquelon, America/Montevideo, America/Paramaribo, America/Punta_Arenas, America/Recife, America/Rosario, America/Santarem, America/Santiago, America/Sao_Paulo, Antarctica/Palmer, Antarctica/Rothera, Atlantic/Stanley, Brazil/East, Chile/Continental, Etc/GMT+3",
-        "value": 180
-    },
-    {
-        "name": "(UTC-02:00) America/Godthab, America/Noronha, America/Nuuk, Atlantic/South_Georgia, Brazil/DeNoronha, Etc/GMT+2",
-        "value": 120
-    },
-    {
-        "name": "(UTC-01:00) America/Scoresbysund, Atlantic/Azores, Atlantic/Cape_Verde, Etc/GMT+1",
-        "value": 60
-    },
-    {
-        "name": "(UTC+00:00) Africa/Abidjan, Africa/Accra, Africa/Bamako, Africa/Banjul, Africa/Bissau, Africa/Conakry, Africa/Dakar, Africa/Freetown, Africa/Lome, Africa/Monrovia, Africa/Nouakchott, Africa/Ouagadougou, Africa/Sao_Tome, Africa/Timbuktu, America/Danmarkshavn, Antarctica/Troll, Atlantic/Canary, Atlantic/Faeroe, Atlantic/Faroe, Atlantic/Madeira, Atlantic/Reykjavik, Atlantic/St_Helena, Eire, Etc/GMT, Etc/GMT+0, Etc/GMT-0, Etc/GMT0, Etc/Greenwich, Etc/UCT, Etc/UTC, Etc/Universal, Etc/Zulu, Europe/Belfast, Europe/Dublin, Europe/Guernsey, Europe/Isle_of_Man, Europe/Jersey, Europe/Lisbon, Europe/London, GB, GB-Eire, GMT, GMT+0, GMT-0, GMT0, Greenwich, Iceland, Portugal, UCT, UTC, Universal, WET, Zulu",
-        "value": 0
-    },
-    {
-        "name": "(UTC+01:00) Africa/Algiers, Africa/Bangui, Africa/Brazzaville, Africa/Casablanca, Africa/Ceuta, Africa/Douala, Africa/El_Aaiun, Africa/Kinshasa, Africa/Lagos, Africa/Libreville, Africa/Luanda, Africa/Malabo, Africa/Ndjamena, Africa/Niamey, Africa/Porto-Novo, Africa/Tunis, Arctic/Longyearbyen, Atlantic/Jan_Mayen, CET, Etc/GMT-1, Europe/Amsterdam, Europe/Andorra, Europe/Belgrade, Europe/Berlin, Europe/Bratislava, Europe/Brussels, Europe/Budapest, Europe/Busingen, Europe/Copenhagen, Europe/Gibraltar, Europe/Ljubljana, Europe/Luxembourg, Europe/Madrid, Europe/Malta, Europe/Monaco, Europe/Oslo, Europe/Paris, Europe/Podgorica, Europe/Prague, Europe/Rome, Europe/San_Marino, Europe/Sarajevo, Europe/Skopje, Europe/Stockholm, Europe/Tirane, Europe/Vaduz, Europe/Vatican, Europe/Vienna, Europe/Warsaw, Europe/Zagreb, Europe/Zurich, MET, Poland",
-        "value": -60
-    },
-    {
-        "name": "(UTC+02:00) Africa/Blantyre, Africa/Bujumbura, Africa/Cairo, Africa/Gaborone, Africa/Harare, Africa/Johannesburg, Africa/Juba, Africa/Khartoum, Africa/Kigali, Africa/Lubumbashi, Africa/Lusaka, Africa/Maputo, Africa/Maseru, Africa/Mbabane, Africa/Tripoli, Africa/Windhoek, Asia/Beirut, Asia/Famagusta, Asia/Gaza, Asia/Hebron, Asia/Jerusalem, Asia/Nicosia, Asia/Tel_Aviv, EET, Egypt, Etc/GMT-2, Europe/Athens, Europe/Bucharest, Europe/Chisinau, Europe/Helsinki, Europe/Kaliningrad, Europe/Kiev, Europe/Kyiv, Europe/Mariehamn, Europe/Nicosia, Europe/Riga, Europe/Sofia, Europe/Tallinn, Europe/Tiraspol, Europe/Uzhgorod, Europe/Vilnius, Europe/Zaporozhye, Israel, Libya",
-        "value": -120
-    },
-    {
-        "name": "(UTC+03:00) Africa/Addis_Ababa, Africa/Asmara, Africa/Asmera, Africa/Dar_es_Salaam, Africa/Djibouti, Africa/Kampala, Africa/Mogadishu, Africa/Nairobi, Antarctica/Syowa, Asia/Aden, Asia/Amman, Asia/Baghdad, Asia/Bahrain, Asia/Damascus, Asia/Istanbul, Asia/Kuwait, Asia/Qatar, Asia/Riyadh, Etc/GMT-3, Europe/Istanbul, Europe/Kirov, Europe/Minsk, Europe/Moscow, Europe/Simferopol, Europe/Volgograd, Indian/Antananarivo, Indian/Comoro, Indian/Mayotte, Turkey, W-SU",
-        "value": -180
-    },
-    {
-        "name": "(UTC+03:30) Asia/Tehran, Iran",
-        "value": -210
-    },
-    {
-        "name": "(UTC+04:00) Asia/Baku, Asia/Dubai, Asia/Muscat, Asia/Tbilisi, Asia/Yerevan, Etc/GMT-4, Europe/Astrakhan, Europe/Samara, Europe/Saratov, Europe/Ulyanovsk, Indian/Mahe, Indian/Mauritius, Indian/Reunion",
-        "value": -240
-    },
-    {
-        "name": "(UTC+04:30) Asia/Kabul",
-        "value": -270
-    },
-    {
-        "name": "(UTC+05:00) Antarctica/Mawson, Asia/Aqtau, Asia/Aqtobe, Asia/Ashgabat, Asia/Ashkhabad, Asia/Atyrau, Asia/Dushanbe, Asia/Karachi, Asia/Oral, Asia/Qyzylorda, Asia/Samarkand, Asia/Tashkent, Asia/Yekaterinburg, Etc/GMT-5, Indian/Kerguelen, Indian/Maldives",
-        "value": -300
-    },
-    {
-        "name": "(UTC+05:30) Asia/Calcutta, Asia/Colombo, Asia/Kolkata",
-        "value": -330
-    },
-    {
-        "name": "(UTC+05:45) Asia/Kathmandu, Asia/Katmandu",
-        "value": -345
-    },
-    {
-        "name": "(UTC+06:00) Antarctica/Vostok, Asia/Almaty, Asia/Bishkek, Asia/Dacca, Asia/Dhaka, Asia/Kashgar, Asia/Omsk, Asia/Qostanay, Asia/Thimbu, Asia/Thimphu, Asia/Urumqi, Etc/GMT-6, Indian/Chagos",
-        "value": -360
-    },
-    {
-        "name": "(UTC+06:30) Asia/Rangoon, Asia/Yangon, Indian/Cocos",
-        "value": -390
-    },
-    {
-        "name": "(UTC+07:00) Antarctica/Davis, Asia/Bangkok, Asia/Barnaul, Asia/Ho_Chi_Minh, Asia/Hovd, Asia/Jakarta, Asia/Krasnoyarsk, Asia/Novokuznetsk, Asia/Novosibirsk, Asia/Phnom_Penh, Asia/Pontianak, Asia/Saigon, Asia/Tomsk, Asia/Vientiane, Etc/GMT-7, Indian/Christmas",
-        "value": -420
-    },
-    {
-        "name": "(UTC+08:00) Asia/Shanghai, Asia/Brunei, Asia/Choibalsan, Asia/Chongqing, Asia/Chungking, Asia/Harbin, Asia/Hong_Kong, Asia/Irkutsk, Asia/Kuala_Lumpur, Asia/Kuching, Asia/Macao, Asia/Macau, Asia/Makassar, Asia/Manila, Asia/Singapore, Asia/Taipei, Asia/Ujung_Pandang, Asia/Ulaanbaatar, Asia/Ulan_Bator, Australia/Perth, Australia/West, Etc/GMT-8, Hongkong, PRC, ROC, Singapore",
-        "value": -480
-    },
-    {
-        "name": "(UTC+08:45) Australia/Eucla",
-        "value": -525
-    },
-    {
-        "name": "(UTC+09:00) Asia/Chita, Asia/Dili, Asia/Jayapura, Asia/Khandyga, Asia/Pyongyang, Asia/Seoul, Asia/Tokyo, Asia/Yakutsk, Etc/GMT-9, Japan, Pacific/Palau, ROK",
-        "value": -540
-    },
-    {
-        "name": "(UTC+09:30) Australia/Darwin, Australia/North",
-        "value": -570
-    },
-    {
-        "name": "(UTC+10:00) Antarctica/DumontDUrville, Asia/Ust-Nera, Asia/Vladivostok, Australia/Brisbane, Australia/Lindeman, Australia/Queensland, Etc/GMT-10, Pacific/Chuuk, Pacific/Guam, Pacific/Port_Moresby, Pacific/Saipan, Pacific/Truk, Pacific/Yap",
-        "value": -600
-    },
-    {
-        "name": "(UTC+10:30) Australia/Adelaide, Australia/Broken_Hill, Australia/South, Australia/Yancowinna",
-        "value": -630
-    },
-    {
-        "name": "(UTC+11:00) Antarctica/Casey, Antarctica/Macquarie, Asia/Magadan, Asia/Sakhalin, Asia/Srednekolymsk, Australia/ACT, Australia/Canberra, Australia/Currie, Australia/Hobart, Australia/LHI, Australia/Lord_Howe, Australia/Melbourne, Australia/NSW, Australia/Sydney, Australia/Tasmania, Australia/Victoria, Etc/GMT-11, Pacific/Bougainville, Pacific/Efate, Pacific/Guadalcanal, Pacific/Kosrae, Pacific/Noumea, Pacific/Pohnpei, Pacific/Ponape",
-        "value": -660
-    },
-    {
-        "name": "(UTC+12:00) Asia/Anadyr, Asia/Kamchatka, Etc/GMT-12, Kwajalein, Pacific/Fiji, Pacific/Funafuti, Pacific/Kwajalein, Pacific/Majuro, Pacific/Nauru, Pacific/Norfolk, Pacific/Tarawa, Pacific/Wake, Pacific/Wallis",
-        "value": -720
-    },
-    {
-        "name": "(UTC+13:00) Antarctica/McMurdo, Antarctica/South_Pole, Etc/GMT-13, NZ, Pacific/Apia, Pacific/Auckland, Pacific/Enderbury, Pacific/Fakaofo, Pacific/Kanton, Pacific/Tongatapu",
-        "value": -780
-    },
-    {
-        "name": "(UTC+13:45) NZ-CHAT, Pacific/Chatham",
-        "value": -825
-    },
-    {
-        "name": "(UTC+14:00) Etc/GMT-14, Pacific/Kiritimati",
-        "value": -840
-    }
+    { value: -765, name: '(UTC+12:45) Chatham Standard Time' },
+    { value: -780, name: '(UTC+13:00) Phoenix Is. Time, Tokelau Time, Tonga Time, West Samoa Standard Time' },
+    { value: -840, name: '(UTC+14:00) Line Is. Time' },
 ];
