c0965735c1c0093b53b686bf6c1d0a1a7d2fc9fb
Elwynn Chen <270001151@qq.com>
2024-03-13 18:13:03 +0800

    fix: pivot table var number limit (#345)

--- packages/graphic-walker/src/utils/index.ts
+++ packages/graphic-walker/src/utils/index.ts
@@ -415,3 +415,15 @@ export function _unstable_encodeRuleValue(value: any) {
     }
     return value;
 }
+
+export function arrayEqual (list1: any[], list2: any[]): boolean {
+    if (list1.length !== list2.length) {
+        return false;
+    }
+    for (let i = 0; i < list1.length; i++) {
+        if (list1[i] !== list2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
