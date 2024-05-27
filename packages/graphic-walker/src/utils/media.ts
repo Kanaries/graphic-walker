bebf209f35e0e3d98ea428924a35bf8edfff2caf
islxyqwe <islxyqwe123@gmail.com>
2024-05-27 11:45:22 +0800

    fix: remove appearence change delay

--- packages/graphic-walker/src/utils/media.ts
+++ packages/graphic-walker/src/utils/media.ts
@@ -1,31 +1,27 @@
-import { useEffect, useState } from "react";
-import { IDarkMode } from "../interfaces";
+import { useEffect, useState } from 'react';
+import { IDarkMode } from '../interfaces';
 
-export function currentMediaTheme(): "dark" | "light" {
-    if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-        return "dark";
+export function currentMediaTheme(): 'dark' | 'light' {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        return 'dark';
     } else {
-        return "light";
+        return 'light';
     }
 }
 
-export function useCurrentMediaTheme(mode: IDarkMode | undefined = 'media'): "dark" | "light" {
-    const [theme, setTheme] = useState<"dark" | "light">(mode === 'media' ? currentMediaTheme() : mode);
+export function useCurrentMediaTheme(mode: IDarkMode | undefined = 'media'): 'dark' | 'light' {
+    const [mediaTheme, setMediaTheme] = useState<'dark' | 'light'>(currentMediaTheme);
 
     useEffect(() => {
-        if (mode === 'media') {
-            const mediaQuery = window.matchMedia?.("(prefers-color-scheme: dark)") as MediaQueryList | undefined;
-            const listener = (e: MediaQueryListEvent) => {
-                setTheme(e.matches ? "dark" : "light");
-            };
-            mediaQuery?.addEventListener("change", listener);
-            return () => {
-                mediaQuery?.removeEventListener("change", listener);
-            };
-        } else {
-            setTheme(mode);
-        }
-    }, [mode]);
+        const mediaQuery = window.matchMedia?.('(prefers-color-scheme: dark)') as MediaQueryList | undefined;
+        const listener = (e: MediaQueryListEvent) => {
+            setMediaTheme(e.matches ? 'dark' : 'light');
+        };
+        mediaQuery?.addEventListener('change', listener);
+        return () => {
+            mediaQuery?.removeEventListener('change', listener);
+        };
+    }, []);
 
-    return theme;
+    return mode === 'media' ? mediaTheme : mode;
 }
