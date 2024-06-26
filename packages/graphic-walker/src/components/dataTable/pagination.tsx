25962d8c710b6bee430ffcaf0810de9423007ae5
islxyqwe <islxyqwe123@gmail.com>
2024-06-26 17:24:11 +0800

    chore: fix pagination

--- packages/graphic-walker/src/components/dataTable/pagination.tsx
+++ packages/graphic-walker/src/components/dataTable/pagination.tsx
@@ -25,38 +25,54 @@ interface PaginationProps {
     extendPageNumber?: number;
 }
 
+export function getShowIndices(total: number, pageIndex: number, pageSize: number, extendPageNumber: number) {
+    const totalPage = Math.ceil(total / (pageSize || 1));
+    const pages = [
+        {
+            index: 0,
+            disabled: false,
+            type: 'page',
+        },
+        ...(new Array(1 + extendPageNumber * 2).fill(0).map((p, i) => ({
+            index: pageIndex - (extendPageNumber - i),
+            disabled: false,
+            type: 'page',
+        })) as IPageItem[]),
+        {
+            index: totalPage - 1,
+            disabled: false,
+            type: 'page',
+        },
+    ].filter((p) => p.index >= 0 && p.index < totalPage) as IPageItem[];
+
+    const pagesUnique: IPageItem[] = [];
+    const indexSet: Set<number> = new Set();
+    for (let p of pages) {
+        if (!indexSet.has(p.index)) {
+            pagesUnique.push(p);
+            indexSet.add(p.index);
+        }
+    }
+    const pageResult: IPageItem[] = pagesUnique.reduce<IPageItem[]>((acc, p) => {
+        if (acc.length === 0) {
+            return [p];
+        }
+        const last = acc[acc.length - 1];
+        if (p.index === last.index + 1) {
+            return [...acc, p];
+        }
+        return [...acc, { index: -1, type: 'placeholder' }, p];
+    }, []);
+    return pageResult;
+}
+
 export default function Pagination(props: PaginationProps) {
     const { total, onNext, onPrev, pageIndex, onPageChange, pageSize = 100, extendPageNumber = 1 } = props;
     const { t } = useTranslation();
-    const showIndices: IPageItem[] = useMemo<IPageItem[]>(() => {
-        const totalPage = Math.ceil(total / (pageSize ?? 1));
-        const pages = [
-            {
-                index: 0,
-                disabled: false,
-                type: 'page',
-            },
-            ...(new Array(1 + extendPageNumber * 2).fill(0).map((p, i) => ({
-                index: pageIndex - (extendPageNumber - i),
-                disabled: false,
-                type: 'page',
-            })) as IPageItem[]),
-            {
-                index: totalPage - 1,
-                disabled: false,
-                type: 'page',
-            },
-        ].filter((p) => p.index >= 0 && p.index < totalPage) as IPageItem[];
-        const pagesUnique: IPageItem[] = [];
-        const indexSet: Set<number> = new Set();
-        for (let p of pages) {
-            if (!indexSet.has(p.index)) {
-                pagesUnique.push(p);
-                indexSet.add(p.index);
-            }
-        }
-        return pagesUnique;
-    }, [pageIndex, pageSize, extendPageNumber, total, pageIndex]);
+    const showIndices: IPageItem[] = useMemo<IPageItem[]>(
+        () => getShowIndices(total, pageIndex, pageSize, extendPageNumber),
+        [pageIndex, pageSize, extendPageNumber, total, pageIndex]
+    );
 
     const pageButton = (index: number) => {
         return (
@@ -85,19 +101,16 @@ export default function Pagination(props: PaginationProps) {
                         {t('actions.prev')}
                     </PaginationPrevious>
                 </PaginationItem>
-                {pageButton(showIndices[0].index)}
-                {showIndices.length > 2 && showIndices[1].index > showIndices[0].index + 1 && (
-                    <PaginationItem>
-                        <PaginationEllipsis />
-                    </PaginationItem>
-                )}
-                {showIndices.slice(1, showIndices.length > 3 ? -1 : undefined).map((page) => pageButton(page.index))}
-                {showIndices.length > 2 && showIndices[showIndices.length - 1].index > showIndices[showIndices.length - 2].index + 1 && (
-                    <PaginationItem>
-                        <PaginationEllipsis />
-                    </PaginationItem>
-                )}
-                {showIndices.length > 2 && pageButton(showIndices[showIndices.length - 1].index)}
+                {showIndices.map((x) => {
+                    if (x.type === 'placeholder') {
+                        return (
+                            <PaginationItem key={x.index}>
+                                <PaginationEllipsis />
+                            </PaginationItem>
+                        );
+                    }
+                    return pageButton(x.index);
+                })}
                 <PaginationItem>
                     <PaginationNext
                         onClick={() => {
