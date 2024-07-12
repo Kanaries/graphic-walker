f5fef00a3a8d28f683e98eb8df438a61b05273dd
islxyqwe <islxyqwe123@gmail.com>
2024-07-12 10:33:24 +0800

    chore: add data clean case

--- packages/playground/src/examples/pages/table.stories.tsx
+++ packages/playground/src/examples/pages/table.stories.tsx
@@ -1,5 +1,5 @@
-import { useContext } from 'react';
-import { TableWalker } from '@kanaries/graphic-walker';
+import { useContext, useMemo } from 'react';
+import { TableWalker, getComputation } from '@kanaries/graphic-walker';
 import { themeContext } from '../context';
 import { useFetch, IDataSource } from '../util';
 
@@ -7,5 +7,28 @@ export default function GraphicWalkerComponent() {
     const { theme } = useContext(themeContext);
     const { dataSource, fields } = useFetch<IDataSource>('https://pub-2422ed4100b443659f588f2382cfc7b1.r2.dev/datasets/ds-students-service.json');
 
-    return <TableWalker fields={fields} data={dataSource} appearance={theme} pageSize={50} />;
+    const computation = getComputation(dataSource);
+
+    const droped = useMemo(() => {
+        const data = dataSource.map((x) => ({ ...x, randomValue: Math.random() }));
+        return data.filter((x) => x.randomValue > 0.5).map((x) => ({ ...x, randomValue: undefined }));
+    }, [dataSource]);
+    const computation2 = getComputation(droped);
+
+    return (
+        <TableWalker
+            fields={fields}
+            data={dataSource}
+            cellStyle={(v, field, row, darkMode) => {
+                if (field.fid.endsWith('_cleaned') && `${v}` !== `${row[field.fid.replace('_cleaned', '')]}`) {
+                    return { backgroundColor: darkMode ? '#a16207' : '#fef9c3' };
+                }
+                if (v === 'none') return { backgroundColor: darkMode ? '#a16207' : '#fef9c3' };
+                return {};
+            }}
+            profilingComputation={[computation, computation2]}
+            appearance={theme}
+            pageSize={50}
+        />
+    );
 }
