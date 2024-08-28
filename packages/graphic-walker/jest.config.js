23e583859d19ed83b513bdfcf4d3ca14556e5aea
islxyqwe <islxyqwe123@gmail.com>
2024-08-28 18:15:10 +0800

    fix: dataTable pagination (#397)
    * fix: pagination button
    
    * fix: button style

--- packages/graphic-walker/jest.config.js
+++ packages/graphic-walker/jest.config.js
@@ -1,5 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+    },
 };
