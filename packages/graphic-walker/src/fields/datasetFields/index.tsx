3572a7b9efad1bad5f45ed725c0f4f89b6e9267f
islxyqwe <islxyqwe123@gmail.com>
2025-04-21 18:01:36 +0800

    fix: height style

--- packages/graphic-walker/src/fields/datasetFields/index.tsx
+++ packages/graphic-walker/src/fields/datasetFields/index.tsx
@@ -1,27 +1,20 @@
 import React from 'react';
 import { Droppable } from '@kanaries/react-beautiful-dnd';
 import { useTranslation } from 'react-i18next';
-import styled from 'styled-components';
 import DimFields from './dimFields';
 import MeaFields from './meaFields';
 import { refMapper } from '../fieldsContext';
-
-const DSContainer = styled.div`
-    @media (min-width: 640px) {
-        height: 680px;
-    }
-`;
 
 const DatasetFields: React.FC = (props) => {
     const { t } = useTranslation('translation', { keyPrefix: 'main.tabpanel.DatasetFields' });
 
     return (
-        <DSContainer className="p-1 sm:mr-0.5 my-0.5 border flex sm:flex-col" style={{ paddingBlock: 0, paddingInline: '0.6em' }}>
+        <div className="p-1 sm:mr-0.5 my-0.5 border flex sm:flex-col sm:max-h-full" style={{ paddingBlock: 0, paddingInline: '0.6em' }}>
             <h4 className="text-xs mb-2 flex-grow-0 cursor-default select-none mt-2">{t('field_list')}</h4>
             <Droppable droppableId="dimensions" direction="vertical">
                 {(provided, snapshot) => (
-                    <div className="flex-shrink min-w-[0px] min-h-[100px] max-h-[380px]" {...provided.droppableProps} ref={refMapper(provided.innerRef)}>
-                        <div className="pd-1 overflow-y-auto h-full">
+                    <div className="flex-shrink min-w-[0px] min-h-[100px] sm:max-h-[380px] sm:overflow-y-auto" {...provided.droppableProps} ref={refMapper(provided.innerRef)}>
+                        <div className="pd-1">
                             <DimFields />
                         </div>
                     </div>
@@ -29,14 +22,14 @@ const DatasetFields: React.FC = (props) => {
             </Droppable>
             <Droppable droppableId="measures" direction="vertical">
                 {(provided, snapshot) => (
-                    <div className="flex-shrink flex-grow min-w-[0px] min-h-[200px]" {...provided.droppableProps} ref={refMapper(provided.innerRef)}>
+                    <div className="flex-shrink flex-grow min-w-[0px] min-h-[200px] sm:overflow-y-auto" {...provided.droppableProps} ref={refMapper(provided.innerRef)}>
                         <div className="border-t flex-grow pd-1 overflow-y-auto h-full">
                             <MeaFields />
                         </div>
                     </div>
                 )}
             </Droppable>
-        </DSContainer>
+        </div>
     );
 };
 
