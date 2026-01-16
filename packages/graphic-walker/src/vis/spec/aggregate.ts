b1ba732ee0bda8013c2f5909cd2fe9f303bbe76b
islxyqwe <islxyqwe123@gmail.com>
2026-01-16 16:24:05 +0800

    fix: aggregate

--- packages/graphic-walker/src/vis/spec/aggregate.ts
+++ packages/graphic-walker/src/vis/spec/aggregate.ts
@@ -8,10 +8,15 @@ export function channelAggregate(encoding: { [key: string]: any }, fields: IView
         if (c.aggregate === null) return;
         const targetField = fields.find((f) => encodeFid(f.fid) === c.field && (f.analyticType === 'measure' || f.fid === COUNT_FIELD_ID));
         if (targetField && targetField.fid === COUNT_FIELD_ID) {
-            c.field = encodeFid(getMeaAggKey(targetField.fid, targetField.aggName))
+            if (!targetField.titleOverride) {
+                c.title = 'Count';
+            }
+            c.field = encodeFid(getMeaAggKey(targetField.fid, targetField.aggName));
         } else if (targetField) {
-            c.title = getMeaAggName(targetField.name, targetField.aggName),
-            c.field = encodeFid(getMeaAggKey(targetField.fid, targetField.aggName))
+            if (!targetField.titleOverride) {
+                c.title = getMeaAggName(targetField.name, targetField.aggName);
+            }
+            c.field = encodeFid(getMeaAggKey(targetField.fid, targetField.aggName));
         }
     });
 }
