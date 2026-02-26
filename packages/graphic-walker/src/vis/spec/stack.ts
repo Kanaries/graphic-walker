0a2f0ac75895a23b4d79d7c4517005838a5b860c
islxyqwe <islxyqwe123@gmail.com>
2026-02-26 20:37:02 +0800

    fix: arc

--- packages/graphic-walker/src/vis/spec/stack.ts
+++ packages/graphic-walker/src/vis/spec/stack.ts
@@ -1,6 +1,10 @@
 import { IStackMode } from "../../interfaces";
 
 export function channelStack(encoding: { [key: string]: any }, stackMode: IStackMode) {
+    if (stackMode === 'stack' && encoding.theta) {
+        encoding.theta.stack = true;
+        return;
+    }
     if (stackMode === 'stack' || stackMode === 'zero') return;
     let stackValue = stackMode === 'none' ? null : stackMode;
     const stackableChannels = ['x', 'y', 'theta', 'radius'];
